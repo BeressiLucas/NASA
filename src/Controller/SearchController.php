367b8772798e5ad048380191993b2d0408c116d2
Darkbloods34 <lucas.beressi34@gmail.com>
2024-04-20 13:52:44 +0200

    [MODIF] add info

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -11,6 +11,16 @@ use Symfony\Contracts\HttpClient\Exception\ExceptionInterface;
 
 class SearchController extends AbstractController
 {
+    /**
+     * @Route("/search", name="search")
+     *Les photos de Curiosity sont intéressantes mais vous devez maintenant récupérer toutes sortes de photos selon la recherche de l'utilisateur.
+     *Pour ce faire vous allez créer un Controller Search dans lequel vous allez créer une route /search qui va retourner une template avec le contenu suivant :
+
+     *Comme pour la récupération des photos de Curiosity, vous en afficherez une aléatoire parmi les résultats de la recherche et vous l'afficherez sur la page.
+     
+     *L'utilisateur doit pouvoir entrer sa recherche dans un input, il utilisera un bouton pour lancer la recherche correspondant à ce qu'il a indiqué dans l'input.
+     */
+
     #[Route('/search', name: 'route_search')]
     public function index(Request $request): Response
     {
@@ -24,8 +34,6 @@ class SearchController extends AbstractController
 
         $returned_data = $this->API_NASA('https://images-api.nasa.gov/search?q=' . $query, 'GET');
 
-        //return $this->json($returned_data['collection']['items'][0]['links']);
-
         $shuffle_number = range(0, count($returned_data['collection']['items']) - 1);
         shuffle($shuffle_number);
         $random_index = $shuffle_number[0];
@@ -35,7 +43,7 @@ class SearchController extends AbstractController
         }
 
         return $this->render('search/search.html.twig', [
-            'title' => 'Recherche sur '.$query,
+            'title' => 'Recherche sur ' . $query,
             'message' => 'Hello Mars!',
             'imageUrl' => $random_photo,
         ]);
